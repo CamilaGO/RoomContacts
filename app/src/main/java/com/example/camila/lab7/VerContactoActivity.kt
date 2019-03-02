b9eb54d3742d0e65b3d13a3933cefa6379630853
Camila <gon18398@uvg.edu.gt>
2019-03-01 22:45:42 -0600

    Implementación en VerContactoActivity para editar la información del contacto

--- app/src/main/java/com/example/camila/lab7/VerContactoActivity.kt
+++ app/src/main/java/com/example/camila/lab7/VerContactoActivity.kt
@@ -1,10 +1,12 @@
 package com.example.camila.lab7
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import com.example.camila.lab7.Data.Contact
 import com.example.camila.lab7.viewmodels.ContactViewModel
@@ -51,6 +53,47 @@ class VerContactoActivity : AppCompatActivity() {
     fun back2Home(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    //Boton para editar la informacion del contacto
+    fun editContact(view: View){
+        var intent = Intent(baseContext, AddEditContactActivity::class.java)
+
+        intent.putExtra(MainActivity.EXTRA_ID, currentId)
+        intent.putExtra(MainActivity.EXTRA_NAME, fillName.text.toString())
+        intent.putExtra(MainActivity.EXTRA_PHONE, fillPhone.text.toString())
+        intent.putExtra(MainActivity.EXTRA_MAIL, fillMail.text.toString())
+        intent.putExtra(MainActivity.EXTRA_PRIORITY, fillPriority.text.toString())
+        startActivityForResult(intent, EDIT_CONTACT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == EDIT_CONTACT_REQUEST && resultCode == Activity.RESULT_OK) {
+            val id = data?.getIntExtra(MainActivity.EXTRA_ID, -1)
+
+            val updateContact = Contact(
+                data!!.getStringExtra(MainActivity.EXTRA_NAME),
+                data.getStringExtra(MainActivity.EXTRA_PHONE),
+                data.getStringExtra(MainActivity.EXTRA_MAIL),
+                data.getIntExtra(MainActivity.EXTRA_PRIORITY, 1)
+            )
+
+            updateContact.id = id!!
+            contactViewModel.update(updateContact)
+
+            //Updating contact
+            fillName.text = updateContact.name
+            fillPhone.text = updateContact.phone
+            fillMail.text = updateContact.mail
+            fillPriority.text = updateContact.priority.toString()
+
+            currentId = updateContact.id
+            Toast.makeText(this, "Contacto actualizado!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Contacto no actualizado!", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
